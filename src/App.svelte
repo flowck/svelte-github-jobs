21b17e1b0193802470909ff48158deffc97e091c
Firmino Changani <firmino.changani@jungle.ai>
2020-02-17 04:49:19 +0000

    Update base url in production

--- src/App.svelte
+++ src/App.svelte
@@ -14,8 +14,11 @@
 	function getJobs(page = 1, description, location) {
 		return new Promise(async (resolve, reject) => {
 			try {
-				console.log(window.location.hostname);
-				const BASE_URL = "http://localhost:5001/jobs";
+				
+				let BASE_URL = "https://svelte-github-jobs.now.sh";
+				if (window.location.hostname === "localhost") {
+					BASE_URL = "http://localhost:5001/jobs";
+				}
 				const query = description || location || fullTime
 					? `?page=${page}&description=${description}&location=${location}&full_time=${fullTime}`
 					: `?page=${page}`;
