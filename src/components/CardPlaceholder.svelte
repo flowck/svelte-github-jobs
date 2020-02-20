e6bbc69dbd9b6edb4340fbd06a59b3dd3104abe4
Firmino Changani <firmino.changani@jungle.ai>
2020-02-20 23:19:12 +0000

    Add a content placeholder component

---
+++ src/components/CardPlaceholder.svelte
@@ -0,0 +1,37 @@
+<script></script>
+
+<style>
+.card-placeholder {
+  width: 100%;
+}
+
+.card-placeholder:not(:last-child) {
+  margin-bottom: 30px;
+}
+
+.card-placeholder__row {
+  width: inherit;
+  height: 10px;
+  background-color: #EEE;
+  animation: isLoading infinite .7s;
+}
+
+.card-placeholder__row:not(:last-child) {
+  margin-bottom: 10px;
+}
+
+@keyframes isLoading {
+  from {
+    background-color: transparent;
+  }
+  to {
+    background-color: #EEE;
+  }
+}
+</style>
+
+<div class="card-placeholder">
+  {#each [0, 1, 2] as row}
+  <div class="card-placeholder__row"></div>
+  {/each}
+</div>
