dfa786f9523190e865a9d665a3fd2e5cb1e9d14c
Firmino Changani <firmino.changani@jungle.ai>
2020-02-20 23:18:54 +0000

    Add a component to show errors

---
+++ src/components/ErrorNotification.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  export let message;
+</script>
+
+<style>
+.error-notification {
+  top: 0;
+  left: 0;
+  width: 100%;
+  color: #FFF;
+  padding: 15px 0;
+  position: fixed;
+  font-size: 14px;
+  background-color: #FA3E3E;
+}
+
+.error-notification .wrap {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.error-notification__close-button {
+  cursor: pointer;
+}
+</style>
+
+<div class="error-notification">
+  <div class="wrap">
+    <div class="error-notification__message">
+      { message }
+    </div>
+
+    <div class="error-notification__close-button" title="Close">
+      <div on:click={ () => dispatch("close") }>
+        <img width="15" height="15" src="/img/close.svg" alt="Close">
+      </div>
+    </div>
+  </div>
+</div>
