b8ebaf55bc4baea53da41d4f6ef40d4031b46314
Firmino Changani <firmino.changani@jungle.ai>
2020-02-20 23:20:16 +0000

    Implement new components

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,8 @@
 	import Filters from "./components/Filters.svelte";
 	import Job from "./components/Job.svelte";
 	import Footer from "./components/Footer.svelte";
+	import CardPlaceholder from "./components/CardPlaceholder.svelte";
+	import ErrorNotification from "./components/ErrorNotification.svelte";
 	import { onMount } from "svelte";
 	import axios from "axios";
 	let jobs = []; 
@@ -9,30 +11,45 @@
 	let description = null;
 	let location = null;
 	let fullTime = null;
+	let isLoading = false;
+	let errorMessage = "";
 
+	/**
+	 * getJobs
+	 * @param {Number} page
+	 * @param {String} description
+	 * @param {String} location
+	 * @returns {Promise}
+	*/
 	function getJobs(page = 1, description, location) {
 		return new Promise(async (resolve, reject) => {
 			try {
-				
 				let BASE_URL = "https://svelte-github-jobs.now.sh/api/jobs";
+
 				if (window.location.hostname === "localhost") {
 					BASE_URL = "http://localhost:5001/jobs";
 				}
+
 				const query = description || location || fullTime
 					? `?page=${page}&description=${description}&location=${location}&full_time=${fullTime}`
 					: `?page=${page}`;
 
 				const response = await axios.get(`${BASE_URL}${query}`);
+			
+				// window.setTimeout(() => {
 				resolve(response.data.jobs);
+				// }, 5000);
 			} catch(error) {
 				reject(error);
 			}
 		});
 	}
 
+	/**
+	 * searchHandler
+	*/
 	async function searchHandler({ detail }) {
 		try {
-
 			// Store search parameters in the app state
 			description = detail.description;
 			location = detail.location;
@@ -40,36 +57,50 @@
 			const requestJobs = await getJobs(page, description, location);
 			jobs = requestJobs;
 		} catch (error) {
-			console.log(error);
+			errorMessage = "Sorry, unfortunately we couldn't search jobs."
 		}
 	};
 
+	/**
+	 * getMoreJobs
+	*/
 	async function getMoreJobs() {
 		try {
 			page += 1;
-			console.log("EX");
 			const requestJobs = await getJobs(page, description, location);
 			const tmp = jobs.concat(requestJobs);
 			jobs = tmp;
 		} catch (error) {
-			console.log(error);
+			errorMessage = "Sorry, unfortunately we couldn't get more jobs. Please try again."
 		}
 	};
 
+	function closeErrorNotification() {
+		errorMessage = "";
+	}
+
 	// On mount
 	onMount(async () => {
+		isLoading = true;
 		try {
-			window.setTimeout(async () => {
-				const requestJobs = await getJobs();
-				jobs = requestJobs;
-			}, 1000);
+			// window.setTimeout(async () => {
+			const requestJobs = await getJobs();
+			jobs = requestJobs;
+			isLoading = false;
+			// }, 500);
 		} catch (error) {
-			console.log(error);
+			isLoading = false;
+			errorMessage = "Sorry, unfortunately load jobs from Github API, please try to refresh the page."
 		}
 	});
 </script>
 
 <main>
+
+	{#if errorMessage.length > 0}
+		<ErrorNotification message={ errorMessage } on:close={ () => closeErrorNotification() } />
+	{/if}
+
 	<div class="content-wrap">
 		<header class="header">
 			<div class="wrap">
@@ -86,26 +117,37 @@
 		</section>
 
 		<section class="jobs-section wrap">
-			{#each jobs as job}
-				<Job
-					title={job.title}
-					date={job.created_at}
-					location={job.location}
-					company={job.company}
-					type={job.type}
-					url={job.url}
-				/>
-			{/each}
-			
-			<div class="more-jobs">
-				<button
-					class="more-jobs__button"
-					on:click={ () => getMoreJobs() }
-				>
-					More Awesome Jobs
-				</button>
-			</div>
 
+			{#if isLoading === true}
+				<div class="card-placeholders">
+					{#each [0, 1, 2, 3, 4, 5, 6] as card}
+						<CardPlaceholder />
+					{/each}
+				</div>
+			{:else}
+				<div class="jobs">
+					{#each jobs as job}
+						<Job
+							title={job.title}
+							date={job.created_at}
+							location={job.location}
+							company={job.company}
+							type={job.type}
+							url={job.url}
+						/>
+					{/each}
+				</div>
+
+				<div class="more-jobs">
+					<button
+						class="more-jobs__button"
+						on:click={ () => getMoreJobs() }
+					>
+						More Awesome Jobs
+					</button>
+				</div>
+			{/if}
+			
 		</section>
 	</div>
 
@@ -167,14 +209,26 @@ main {
 	border: 4px solid rgba(0, 0, 0, .1);
 }
 
+.more-jobs {
+	margin-top: 20px;
+}
+
 .more-jobs__button {
+	width: 100%;
 	border: none;
 	color: #FFF;
+	height: 30px;
+	padding: 0 20px;
 	font-size: 12px;
+	cursor: pointer;
 	font-weight: bold;
 	border-radius: 3px;
 	text-shadow: -1px -1px 0 #2371a3;
 	background: -webkit-gradient(linear, left top, left bottom, from(#46a9e7), to(#1e81bf));
+}
+
+.more-jobs__button:hover {
+	background: -webkit-gradient(linear, left top, left bottom, from(#2b91e2), to(#125db3));
 }
 
 @media screen and (max-width: 900px) {
