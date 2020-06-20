c186b50580d0143d4c2134b213a12eacf6d0ffce
Firmino Changani <firmino.changani@mercedes-benz.io>
2020-06-20 18:37:41 +0100

    feat: Create header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,29 @@
+<script></script>
+
+<header class="header">
+  <div class="wrap">
+    <h1 class="header__title">
+      <b>Github</b> Jobs
+    </h1>
+  </div>
+</header>
+
+<style>
+.header {
+	height: 50px;
+	color: #FFF;
+	margin-bottom: 25px;
+	background-color: #2B7FC3;
+	border-top: 4px solid #1D5C8F;
+}
+
+.header__title {
+	font-size: 26px;
+	font-weight: 100;
+	margin-top: 6px;
+}
+
+.header__title b{
+	font-weight: bold;
+}
+</style>
